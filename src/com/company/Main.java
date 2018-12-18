97526d1006d6310040f65f7b5f9d0e1487e4076b
raina garg <gargraina03@gmail.com>
2018-12-18 13:45:47 +0530

    new commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        CityNode city1 = new CityNode(2, 3);
-        CityNode city2 = new CityNode(5, 7);
-        System.out.println(city1.distanceTo(city2));
-
 
     }
 }
--- src/com/company/SimulatedAnnealing.java
+++ src/com/company/SimulatedAnnealing.java
@@ -1,4 +1,33 @@
 package com.company;
 
+
+import java.util.ArrayList;
+
+/**
+ * The main simulated annealing class
+ * requires 3 parameters - starting temperature, cooling rate, int numberOfIterations
+ */
 public class SimulatedAnnealing {
+
+    //generate a new Travel Mode Instance
+    TravelModel travel = new TravelModel(10);
+
+    //constructor for the Simulated Annealing Class
+
+
+    public ArrayList<CityNode> simulate(double initTemperature, double alpha, double thresholdTemp, int numOfItertations) {
+        travel.shuffleList();
+        double optimisedDistance = 0.0;
+        ArrayList<CityNode> optSequence = new ArrayList<>();
+        for (int i = 0; i < numOfItertations; i++) {
+
+        }
+
+
+        return optSequence;
+
+
+    }
+
+
 }
